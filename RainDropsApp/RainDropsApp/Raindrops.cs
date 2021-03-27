3c23b87fc2c66704bd5211f62326aea336db1afa
Kim Waters <kimwaters1996@gmail.com>
2021-03-27 19:25:23 +0000

    Working method with no validation

--- RainDropsApp/RainDropsApp/Raindrops.cs
+++ RainDropsApp/RainDropsApp/Raindrops.cs
@@ -6,12 +6,33 @@ namespace RainDropsApp
     {
         public static string GetRaindropsString(int n)
         {
-            return n.ToString();
+            string raindropString = "";
+
+            if (n % 3 == 0)
+            {
+                raindropString += "Pling";
+            }
+            if (n % 5 == 0)
+            {
+                raindropString += "Plang";
+            }
+            if (n % 7 == 0)
+            {
+                raindropString += "Plong";
+            }
+            if(raindropString == "")
+            {
+                raindropString = n.ToString();
+            }
+
+            return raindropString;
         }
 
         static void Main(string[] args)
         {
             Console.WriteLine(GetRaindropsString(28));
+            Console.WriteLine(GetRaindropsString(30));
+            Console.WriteLine(GetRaindropsString(34));
         }
     }
 }
