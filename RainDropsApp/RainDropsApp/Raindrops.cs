d80d9f3022abdca06560a981c43f9a876cd26276
Kim Waters <kimwaters1996@gmail.com>
2021-03-27 19:19:24 +0000

    Method skeleton implemented

---
+++ RainDropsApp/RainDropsApp/Raindrops.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RainDropsApp
+{
+    public class Raindrops
+    {
+        public static string GetRaindropsString(int n)
+        {
+            return n.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetRaindropsString(28));
+        }
+    }
+}
