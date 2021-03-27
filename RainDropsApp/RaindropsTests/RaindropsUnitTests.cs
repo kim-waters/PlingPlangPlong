d6b2125992f2eb91056aa0c1a8f3f072c9dd2bf0
Kim Waters <kimwaters1996@gmail.com>
2021-03-27 19:51:44 +0000

    Unit tests for negative integers, and a failing test case to fix for when zero is passed to the method

--- RainDropsApp/RaindropsTests/RaindropsUnitTests.cs
+++ RainDropsApp/RaindropsTests/RaindropsUnitTests.cs
@@ -14,7 +14,23 @@ namespace RainDropsTests
         [TestCase(105, "PlingPlangPlong")]
         [TestCase(121550625, "PlingPlangPlong")]
         [TestCase(34, "34")]
+        [TestCase(0, "0")]
         public void WhenAValidIntegerIsPassedToTheMethod_TheCorrectStringIsReturned(int num, string expectedString)
+        {
+            var result = Raindrops.GetRaindropsString(num);
+            Assert.That(result, Is.EqualTo(expectedString));
+        }
+
+        [TestCase(-6, "Pling")]
+        [TestCase(-10, "Plang")]
+        [TestCase(-28, "Plong")]
+        [TestCase(-30, "PlingPlang")]
+        [TestCase(-42, "PlingPlong")]
+        [TestCase(-70, "PlangPlong")]
+        [TestCase(-105, "PlingPlangPlong")]
+        [TestCase(-121550625, "PlingPlangPlong")]
+        [TestCase(-34, "34")]
+        public void WhenANegativeIntegerIsPassedToTheMethod_TheCorrectStringIsStillReturned(int num, string expectedString)
         {
             var result = Raindrops.GetRaindropsString(num);
             Assert.That(result, Is.EqualTo(expectedString));
