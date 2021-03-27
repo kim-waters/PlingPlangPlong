7c741f60a0e79a665614a3ba9ee0cc319c55d96f
Kim Waters <kimwaters1996@gmail.com>
2021-03-27 19:48:48 +0000

    More test cases added for the unit test, covering all possible outputs

--- RainDropsApp/RaindropsTests/RaindropsUnitTests.cs
+++ RainDropsApp/RaindropsTests/RaindropsUnitTests.cs
@@ -5,12 +5,15 @@ namespace RainDropsTests
 {
     public class Tests
     {
+        [TestCase(6, "Pling")]
+        [TestCase(10, "Plang")]
         [TestCase(28, "Plong")]
         [TestCase(30, "PlingPlang")]
-        [TestCase(34, "34")]
+        [TestCase(42, "PlingPlong")]
+        [TestCase(70, "PlangPlong")]
         [TestCase(105, "PlingPlangPlong")]
-        [TestCase(-105, "PlingPlangPlong")]
         [TestCase(121550625, "PlingPlangPlong")]
+        [TestCase(34, "34")]
         public void WhenAValidIntegerIsPassedToTheMethod_TheCorrectStringIsReturned(int num, string expectedString)
         {
             var result = Raindrops.GetRaindropsString(num);
